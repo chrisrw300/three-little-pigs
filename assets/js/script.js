6918b6aa0a9357b1a183311e8c8d0f3df0a8a39c
chrisrw300 <chrisrw300@gmail.com>
2021-10-19 19:49:24 -0600

    switched to Bootstrap for nav, added hero image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,1 @@
-// Responsive Navbar Toggle
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-})
-
-// Dropdown Menu
+// Start
